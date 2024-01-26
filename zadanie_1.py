419758983cf32ad68417eaabb301454c5e6bc42e
GrzegorzTreter <107022065+g7eg@users.noreply.github.com>
2024-01-26 17:10:27 +0100

    Create zadanie_1.py

---
+++ zadanie_1.py
@@ -0,0 +1,22 @@
+# Wpisz tutaj nr swojego stanowiska: 123
+# Podaj nr indeksu: 123456
+
+# 1. Napisać program, który składa się z funkcji przyjmującej jako argumenty dwie liczby całkowite x i y,
+# następnie oblicza sumę wszystkich liczb nieparzystych znajdujących się w przedziale od x do y (włącznie).
+# Program powinien pobierać argumenty x i y od użytkownika,
+# w celu zabezpieczenia przed wprowadzeniem niepoprawnych danych, wykorzystać obsługę wyjątków.
+# Program powinien na koniec wyświetlić wynik. 
+
+# Przykład danych do przetestowania działania programu:
+# Input:
+#     Podaj wartość x: 1
+#     Podaj wartość y: 10
+# Output:
+#     Suma wszystkich liczb nieparzystych: 25
+
+
+
+# Nie zmieniaj nazwy funkcji
+def return_sum_of_odd_numer():
+  sum = 0
+  return sum
